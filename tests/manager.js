1dd8aecb3a60a4c133004d5efc4360660bb55cd1
giovanebribeiro <giovanebribeiro@gmail.com>
2015-04-11 14:09:41 -0300

    finish TaskManager - CRUD part.

--- tests/manager.js
+++ tests/manager.js
@@ -11,35 +11,53 @@ var TaskManager = require('../lib/TaskManager.js');
 
 suite('TaskManager tests', function(){
   var manager; 
+  var tomorrow;
   
   setup(function(){
+    tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
     // scheduler must be started.
     manager = new TaskManager();
     //manager.start();
   });
 
-  test(' - add a task to be executed every second.', function(done){
-    var tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
+  test(' - tasks CRUD', function(done){
+    //
+    // Testing the exceptions thrown
+    // The assert.throws function have 3 parameters:
+    // - The function to test, wrapped in another function to receive args
+    // - The Error type
+    // - The Error message, to compare.
+ 
+    // no parameters
+     assert.throws(function(){
+       manager.createOrUpdate();
+     }, Error,  'The task name is required, at least');
 
+     // insufficient parameters for new task
+     assert.throws(function(){
+      manager.createOrUpdate("hello");
+     }, Error, 'Insufficient parameters');
+
+     // wrong type parameter
+     assert.throws(function(){
+      manager.createOrUpdate("hello", "* * * * *", "wrong type function", tomorrow);
+     }, Error, "The task activity must be a function");
+
+
+    // Create a new TaskData...
     manager.createOrUpdate('hello', '* * * * *', function(){ console.log("Hello World!!"); }, tomorrow);
+    assert(manager.tasksSaved.hello.name == "hello", "The Task Data is not saved properly.");
 
-    assert(manager.tasksData.hello.name == "hello", "The Task Data is not saved properly.");
+    // ... update this object ..
+    manager.createOrUpdate('hello', '*/2 * * * *');
+    assert(manager.tasksSaved.hello.cron.string == '*/2 * * * *');
 
+    // ... then remove it.
+    manager.remove('hello');
+    assert.isUndefined(manager.tasksSaved.hello, "The task is not removed.");
     done();
 
   });
 
-  /*test(' - update the cron string', function(done){
-    // update a task with specific name. 
-    // Change the dates to execute (every minute.)
-    // check if file changes
-    done();
-  });*/
-
-  /*test('remove task', function(done){
-    // Remove the task from specific name.
-    // check if task filename was removed.
-    done();
-  });*/
 });
