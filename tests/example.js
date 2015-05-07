bec881cfb0e2be76fb8035416ac7800a1b0eef0e
giovanebribeiro <giovanebribeiro@gmail.com>
2015-05-06 21:42:59 -0300

    Refactoring of TaskRunner: taking tasks from TaskManager

--- tests/example.js
+++ tests/example.js
@@ -22,15 +22,16 @@ setTimeout(function(){
 }, 65000);
 */
 
-// exemplo 2
+// example 2
 var TaskRunner = require('../lib/TaskRunner.js');
 var TaskManager = require('../lib/TaskManager.js');
 
 var tm = new TaskManager();
-var tr = new TaskRunner(tm);
 
 tm.loadTasks(function(err, tasksSaved){
   if(err) throw err;
+
+  var tr = new TaskRunner(tm);
 
   // start task (every minute, with end date to 5 minutes)
   tr.startTask('hello');
@@ -39,7 +40,7 @@ tm.loadTasks(function(err, tasksSaved){
   tm.createTask('hello2', function(callback){
     console.log("Hello World from hello2!! "+new Date());
     callback();
-  }, '0 */3 * * * *', new Date("Wed May 0 2015 20:00:00 GMT-0300 (BRT)"), function(err, taskData){
+  }, '0 */3 * * * *', new Date("Wed May 6 2015 23:00:00 GMT-0300 (BRT)"), function(err, taskData){
     if(err) throw err;
 
     tr.startTask("hello2");
