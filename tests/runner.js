6e001a4e8155595efd21bc0834e3a2f0d55820b0
Giovane Boaviagem <giovanebribeiro@gmail.com>
2015-06-09 18:52:23 -0300

    #1 - Including personal exit codes

--- tests/runner.js
+++ tests/runner.js
@@ -3,6 +3,7 @@ var assert = chai.assert;
 
 var TaskManager = require('../lib/TaskManager.js');
 var TaskRunner = require('../lib/TaskRunner.js');
+var TaskConstraints = require('../lib/TaskConstraints.js');
 
 suite('TaskRunner tests', function(){
   var runner;
@@ -17,6 +18,8 @@ suite('TaskRunner tests', function(){
     runner = new TaskRunner(manager);
   });
 
+
+
   test('- if the task is executed 3 times and stops.', function(done){
     // set timeout to delay
     var delayEndDate = 5 * 60 * 1000; // 5 minute in milisseconds
@@ -25,18 +28,21 @@ suite('TaskRunner tests', function(){
     var count = 0;
 
     var runnerListener =  function(type, pid, data){
-      if(type == "task_loop" && data.code === 0){
+      if(type == "task_loop" && data.code > 9){ // 9 is the max reserved exit code
 
         if(count < 2){
           count++;
         }else{
+          console.log(" Stopping the task. count = "+count);
           runner.stop('hello');
          
-          // only in the next delay, we can check if task will stop to execute. 
+          // only in the next delay, we can check if task will stop to execute.
+          console.log("Waiting the next delay to check"); 
           setTimeout(function(){
+            console.log("Checking..");
             assert(!runner.isRunning('hello'), "The task is still running after stops.");
             runner.removeListener('runner', runnerListener);
-            done();
+            return done();
           }, 1*60*1000);
         }
 
@@ -50,7 +56,7 @@ suite('TaskRunner tests', function(){
     var name = "hello";
     var activity = function(callback){
      console.log("Hello World from hello (task runner)!!");
-     callback();
+     callback(null, 10);
     };
     var cronFreq = '0 * * * * *';
 
@@ -64,4 +70,60 @@ suite('TaskRunner tests', function(){
     }); 
     
   });
+
+
+  test('- testing error codes.', function(done){
+    var delayEndDate = 5 * 60 * 1000; // 5 min
+    this.timeout(delayEndDate);
+
+    var count = 0;
+    var customExitCode = 10;
+
+    var runnerListener = function(type, pid, data){
+      if(type == 'task_exit'){
+        assert(data.code === TaskConstraints.exit_codes.loop_ends, "The task not have the correct exit code when task ends");
+        
+        setTimeout(function(){
+          runner.removeListener('runner', runnerListener);
+          done();
+        }, 1 * 60 * 1000);
+
+      }else if(type == 'task_loop'){
+
+        // 0 is the only exit code that not implies a loop.
+        assert(data.code > 0, "The task not have the correct exit codes when task loops");
+
+        // assert only to check if personalized exit code is correct.
+        if( data.code > 9 ){
+          assert(data.code === customExitCode, "The task not have the specified exit code");
+        }
+
+        
+      }
+    };
+
+
+    // implementing the listener for the TaskRunner
+    runner.on('runner',runnerListener);
+
+    // creating a task
+    var name = "hello2";
+    var activity = function(callback){
+     console.log("Hello World from hello2 (task runner)!!");
+     callback(null, 10);
+    };
+    var cronFreq = '0 * * * * *';
+
+    var startDate = Date.now(); 
+    var endDate = new Date(startDate + (2 * 60 * 1000)); // the task will over in 2 minutes
+
+    manager.createTask(name, activity, cronFreq, endDate, function(err, taskData){
+     if(err) throw err;
+
+     runner.startTask(name);    
+    });
+
+  });
+
+
 });
